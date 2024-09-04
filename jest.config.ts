f6373407cf12177dd68ee7c73e11411ef34b2ebd
Aditi Chandra <aditi@strategywerks.com>
2024-09-04 15:48:01 +0530

    fixed: jest config

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+export { };
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts',
+    '!**/vendor/**'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  },
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/coverage",
+    "package.json",
+    "package-lock.json",
+    "reportWebVitals.ts",
+    "setupTests.ts",
+    "index.tsx"
+  ],
+   moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/style.mock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+}
